1de422034b91d75fb1b422ff7b783f9297db021a
Dequé <carolinedeque@gmail.com>
2022-07-08 10:15:04 +0200

    Creation fichier data

---
+++ src/data/data.js
@@ -0,0 +1,48 @@
+import ChatIcon from "../assets/icon-chat.png";
+import MoneyIcon from "../assets/icon-money.png";
+import SecurityIcon from "../assets/icon-security.png";
+
+/**
+ * return features data homepage
+ */
+export const features = [
+    {
+        icon: ChatIcon,
+        altContent: "Chat Icon",
+        title: "You are our #1 priority",
+        text: " Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.",
+    },
+    {
+        icon: MoneyIcon,
+        altContent: "Money Icon",
+        title: "More savings means higher rates",
+        text: "The more you save with us, the higher your interest rate will be!",
+    },
+    {
+        icon: SecurityIcon,
+        altContent: "Security Icon",
+        title: "Security you can trust",
+        text: "We use top of the line encryption to make sure your data and money is always safe.",
+    },
+];
+
+/**
+ * returns user data to profile page
+ */
+export const userAccountData = [
+    {
+        title: "Argent Bank Checking (x8349)",
+        amount: "$2,082.79",
+        description: "Available Balance",
+    },
+    {
+        title: "Argent Bank Savings (x6712)",
+        amount: "$10,928.42",
+        description: "Available Balance",
+    },
+    {
+        title: "Argent Bank Credit Card (x8349)",
+        amount: "$184.30",
+        description: "Current Balance",
+    },
+];
