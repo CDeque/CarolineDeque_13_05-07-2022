800419caefd8f5ad576bacb68b4b79d059cac230
Dequé <carolinedeque@gmail.com>
2022-07-08 10:14:09 +0200

    Creation BalanceCard

---
+++ src/components/BalanceCard/index.jsx
@@ -0,0 +1,90 @@
+//------ Imports------//
+import styled from "styled-components";
+import { PropTypes } from "prop-types";
+
+export default function BalanceCard(data) {
+    return (
+        <AccountSection className="account">
+            <AccountWrapper className="account-content-wrapper">
+                <AccountTitle className="account title">
+                    {data.title}
+                </AccountTitle>
+                <AccountAmount className="account-amount">
+                    {data.amount}
+                </AccountAmount>
+                <AccountDescription className="account-amount-description">
+                    {data.description}
+                </AccountDescription>
+            </AccountWrapper>
+            <AccountWrapper className="account-content-wrapper cta">
+                <TransactionButton className="transaction-button">
+                    view transactions
+                </TransactionButton>
+            </AccountWrapper>
+        </AccountSection>
+    );
+}
+BalanceCard.propTypes = {
+    title: PropTypes.string.isRequired,
+    amount: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+};
+const AccountSection = styled.section`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border: 1px solid black;
+    background-color: #fff;
+    width: 80%;
+    margin: 0 auto;
+    flex-direction: column;
+    padding: 1.5rem;
+    box-sizing: border-box;
+    text-align: left;
+    margin-bottom: 2rem;
+    @media (min-width: 720px) {
+      .account {
+        flex-direction: row;
+      }
+`;
+
+const AccountAmount = styled.p`
+    margin: 0;
+    font-size: 2.5rem;
+    font-weight: bold;
+`;
+
+const AccountDescription = styled.p`
+    margin: 0;
+`;
+const AccountTitle = styled.h3`
+    margin: 0;
+    padding: 0;
+    font-size: 1rem;
+    font-weight: normal;
+`;
+const AccountWrapper = styled.div`
+    width: 100%;
+    flex: 1;
+
+    @media (min-width: 720px) {
+        &.account-content-wrapper.cta {
+            flex: 0;
+        }
+    }
+`;
+
+const TransactionButton = styled.button`
+    display: block;
+    width: 100%;
+    padding: 8px;
+    font-size: 1.1rem;
+    font-weight: bold;
+    margin-top: 1rem;
+    border-color: #00bc77;
+    background-color: #00bc77;
+    color: #fff;
+    @media (min-width: 720px) {
+        width: 200px;
+    }
+`;
--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -1,7 +1,13 @@
 //------ Imports ------//
 
 import styled from "styled-components";
+import BalanceCard from "../../components/BalanceCard";
+import { userAccountData } from "../../data/data";
 
+/**
+ *
+ * @returns profile page
+ */
 export default function User() {
     return (
         <Main className="main bg-dark">
@@ -13,58 +19,21 @@ export default function User() {
                 <EditButton className="edit-button">Edit Name</EditButton>
             </Header>
             <H2 className="sr-only">Accounts</H2>
-            <AccountSection className="account">
-                <AccountWrapper className="account-content-wrapper">
-                    <AccountTitle className="account title">Data</AccountTitle>
-                    <AccountAmount className="account-amount">
-                        amount
-                    </AccountAmount>
-                    <AccountDescription className="account-amount-description">
-                        Description
-                    </AccountDescription>
-                </AccountWrapper>
-                <AccountWrapper className="account-content-wrapper cta">
-                    <TransactionButton className="transaction-button">
-                        view transactions
-                    </TransactionButton>
-                </AccountWrapper>
-            </AccountSection>
-            <AccountSection className="account">
-                <AccountWrapper className="account-content-wrapper">
-                    <AccountTitle className="account title">Data</AccountTitle>
-                    <AccountAmount className="account-amount">
-                        amount
-                    </AccountAmount>
-                    <AccountDescription className="account-amount-description">
-                        Description
-                    </AccountDescription>
-                </AccountWrapper>
-                <AccountWrapper className="account-content-wrapper cta">
-                    <TransactionButton className="transaction-button">
-                        view transactions
-                    </TransactionButton>
-                </AccountWrapper>
-            </AccountSection>
-            <AccountSection className="account">
-                <AccountWrapper className="account-content-wrapper">
-                    <AccountTitle className="account title">Data</AccountTitle>
-                    <AccountAmount className="account-amount">
-                        amount
-                    </AccountAmount>
-                    <AccountDescription className="account-amount-description">
-                        Description
-                    </AccountDescription>
-                </AccountWrapper>
-                <AccountWrapper className="account-content-wrapper cta">
-                    <TransactionButton className="transaction-button">
-                        view transactions
-                    </TransactionButton>
-                </AccountWrapper>
-            </AccountSection>
+            {userAccountData.map((data, index) => (
+                <BalanceCard
+                    key={index}
+                    title={data.title}
+                    amount={data.amount}
+                    description={data.description}
+                />
+            ))}
         </Main>
     );
 }
 
+/**
+ * page style
+ */
 const Main = styled.main`
     background-color: #12002b;
     flex: 1;
@@ -93,64 +62,4 @@ const H2 = styled.h2`
     position: absolute !important;
     width: 1px !important;
     white-space: nowrap !important; /* 3 */
-`;
-
-const AccountSection = styled.section`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    border: 1px solid black;
-    background-color: #fff;
-    width: 80%;
-    margin: 0 auto;
-    flex-direction: column;
-    padding: 1.5rem;
-    box-sizing: border-box;
-    text-align: left;
-    margin-bottom: 2rem;
-    @media (min-width: 720px) {
-      .account {
-        flex-direction: row;
-      }
-`;
-
-const AccountAmount = styled.p`
-    margin: 0;
-    font-size: 2.5rem;
-    font-weight: bold;
-`;
-
-const AccountDescription = styled.p`
-    margin: 0;
-`;
-const AccountTitle = styled.h3`
-    margin: 0;
-    padding: 0;
-    font-size: 1rem;
-    font-weight: normal;
-`;
-const AccountWrapper = styled.div`
-    width: 100%;
-    flex: 1;
-
-    @media (min-width: 720px) {
-        &.account-content-wrapper.cta {
-            flex: 0;
-        }
-    }
-`;
-
-const TransactionButton = styled.button`
-    display: block;
-    width: 100%;
-    padding: 8px;
-    font-size: 1.1rem;
-    font-weight: bold;
-    margin-top: 1rem;
-    border-color: #00bc77;
-    background-color: #00bc77;
-    color: #fff;
-    @media (min-width: 720px) {
-        width: 200px;
-    }
 `;
